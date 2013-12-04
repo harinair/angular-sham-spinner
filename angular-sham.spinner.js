bc7f9cb3d4d2de918fb702471f03e07830ad28dc
Hari Gangadharan <hari_g@hotmail.com>
2013-12-04 00:12:06 -0800

    Added option to disable spinner

--- angular-sham.spinner.js
+++ angular-sham.spinner.js
@@ -47,10 +47,12 @@ app.factory('AngularShamInterceptor', [
     }]
 );
 
-app.factory('AngularShamNotification', ['$rootScope', function($rootScope){
+app.factory('AngularShamNotification', ['$rootScope', '$timeout', function($rootScope, $timeout){
     // private notification messages
     var _START_REQUEST_ = 'angularShamNotification:_START_REQUEST_';
     var _END_REQUEST_ = 'angularShamNotification:_END_REQUEST_';
+    var _disabled = false;
+    var _lastTimeout = null;
 
     return {
         /**
@@ -59,7 +61,9 @@ app.factory('AngularShamNotification', ['$rootScope', function($rootScope){
          * HTTP interceptor.
          */
         requestStarted: function() {
-            $rootScope.$broadcast(_START_REQUEST_);
+            if (!_disabled) {
+                $rootScope.$broadcast(_START_REQUEST_);
+            }
         },
 
         /**
@@ -93,6 +97,36 @@ app.factory('AngularShamNotification', ['$rootScope', function($rootScope){
             $rootScope.$on(_END_REQUEST_, function(event){
                 handler(event);
             });
+        },
+
+        /**
+         * This method will disable sham spinner for the reset time. After
+         * reset time the spinner will be enabled again. If reset time is
+         * set to less than 0 then to enable spinner, call this method
+         * again with false as the disable argument.
+         *
+         * @param disable       Set to true to disable the sham spinner
+         * @param resetTime     reset time in ms (no reset if set to < 0)
+         */
+        setDisabled: function(disable, resetTime) {
+            _disabled = disable;
+            if (_disabled) {
+                this.requestEnded();
+                if (resetTime > 0) {
+                    if (_lastTimeout !== null) {
+                        $timeout.cancel(_lastTimeout);
+                    }
+                    _lastTimeout = $timeout(function() {
+                        _disabled = false;
+                        _lastTimeout = null;
+                    }, resetTime);
+                }
+            } else {
+                if (_lastTimeout !== null) {
+                    $timeout.cancel(_lastTimeout);
+                    _lastTimeout = null;
+                }
+            }
         }
     };
 }]);
